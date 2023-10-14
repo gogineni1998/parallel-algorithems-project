d82713d36bb7612706c2ecbcd11dc123a3607ded
gogineni1998 <gogineni1998@gmail.com>
2023-10-14 18:39:14 -0400

    Update Best_Day_To_Buy_And_Sell_lthe_Stock.c

--- Best_Day_To_Buy_And_Sell_lthe_Stock.c
+++ Best_Day_To_Buy_And_Sell_lthe_Stock.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define n 10
+#define n 12
 
-int a[] = {111, 123, 2343, 234, 5, 26, 17, 78, 49, 100};
+int a[] = {111, 123, 2343, 234, 5, 26, 17, 78, 49, 100,1,1000000};
 
 int a2[1000];
 
 int main(int argc, char *argv[])
 {
-    int pid, np, elements_per_process, n_elements_recived, best_price;
+    int pid, np, elements_per_process, n_elements_recived, best_price = -1;
     MPI_Status status;
 
     MPI_Init(&argc, &argv);
@@ -24,11 +24,18 @@ int main(int argc, char *argv[])
         elements_per_process = n / np;
         if(elements_per_process == 1) {
             min_value = a[0];
+            best_price = min_value;
         }
         else {
             for (i = 0; i < elements_per_process; i++) {
                 if(min_value > a[i]) {
                     min_value = a[i];
+                }
+                else {
+                    int temp = a[i] - min_value;
+                    if(temp > best_price) {
+                        best_price = temp;
+                    }
                 }
             }
         }
@@ -49,15 +56,21 @@ int main(int argc, char *argv[])
             MPI_Send(&elements_left, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
             MPI_Send(&a[index], elements_left, MPI_INT, i, 0, MPI_COMM_WORLD);
             int tmp;
-            MPI_Recv(&tmp, 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
-            printf("imp %d\n",tmp);
-            if(min_value > tmp) {
-                min_value = tmp;
+            // MPI_Recv(&tmp, 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
+            // printf("imp %d\n",tmp);
+            // if(min_value > tmp) {
+            //     min_value = tmp;
+            // }
+            for(int i=1;i<np;i++) {
+                MPI_Recv(&tmp, 1, MPI_INT, MPI_ANY_SOURCE, 2, MPI_COMM_WORLD, &status);
+                if(tmp > best_price) {
+                    best_price = tmp;
+                }
             }
-            printf("Minimum value of array is : ******** %d ***********\n\n", min_value);
+            printf("best price value of array is : %d\n\n", best_price);
         }
         else {
-            printf("Minimum value of array is : %d\n\n", min_value);
+            printf("best price value of array is : %d\n\n", best_price);
         }
     }
     else
@@ -82,7 +95,14 @@ int main(int argc, char *argv[])
             if(partial_array_minValue > a2[i]) {
                 partial_array_minValue = a2[i];
             }
+            else {
+                int temp = a2[i] - partial_array_minValue;
+                if(temp > best_price) {
+                    best_price = temp;
+                }
+            }
         }
+        MPI_Send(&best_price, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
         printf("Partial min value calculated by process %d : %d\n", pid, partial_array_minValue);
         if (pid < np - 1)
         {
